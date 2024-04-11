6cd4ee5660afd6f88c614f1706667e0fe42235cc
kei ito <ito.kei@jvckenwood.com>
2024-04-11 11:20:52 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Reactive.Bindings;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ReactivePropertySlim<int> AAAVal { get; }
+
+        public ReactivePropertySlim<string> BBBVal { get; }
+
+
+        public MainWindow()
+        {
+            AAAVal = new ReactivePropertySlim<int>(10);
+
+            BBBVal = new ReactivePropertySlim<string>("init value");
+
+            InitializeComponent();
+        }
+    }
+}
