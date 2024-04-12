560932d0c426e02a0f47ba6e7a65265ff6b61de9
kei ito <ito.kei@jvckenwood.com>
2024-04-12 10:22:48 +0900

    ZZZ設定を追加

--- WpfApp1/AAAEntity/AAAEntity.cs
+++ WpfApp1/AAAEntity/AAAEntity.cs
@@ -2,18 +2,48 @@
 {
     public class AAAEntity
     {
+        public ZZZVO ZZZ { get; private set; }
+
         public AAAVO AAA { get; private set; }
 
         public AAAEntity()
         {
+            ZZZ = new(20);
             AAA = new(10);
+        }
+
+        public void SetZZZ(ZZZVO zzz, IAAAChangedEvent changedEvent)
+        {
+            ZZZ = zzz;
+
+            if (IsHaveToCorrectAAA(zzz.Value))
+            {
+                SetAAA(new(zzz.Value), changedEvent);
+            }
+        }
+
+        public bool IsHaveToCorrectAAA(int zzz)
+        {
+            return zzz < AAA.Value;
         }
 
         public void SetAAA(AAAVO aaa, IAAAChangedEvent changedEvent)
         {
-            AAA = aaa;
+            if (IsOverZZZ(aaa.Value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(aaa), "範囲外");
+            }
+            else
+            {
+                AAA = aaa;
+            }
 
             changedEvent.Execute();
+        }
+
+        public bool IsOverZZZ(int aaa)
+        {
+            return ZZZ.Value < aaa;
         }
 
         public AAAEntity Clone()
--- WpfApp1/UI/UiWindow/MainWindow/MainWindowViewModel.cs
+++ WpfApp1/UI/UiWindow/MainWindow/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using Reactive.Bindings.Extensions;
 
 using System.ComponentModel;
 using System.Reactive.Disposables;
+using System.Windows;
 
 
 namespace UI.UiWindow.MainWindow
@@ -17,6 +18,8 @@ namespace UI.UiWindow.MainWindow
 
         private readonly CompositeDisposable _compositeDisposable = [];
 
+        public ReactivePropertySlim<int> ZZZVal { get; private set; } = new(0);
+
         public ReactivePropertySlim<int> AAAVal { get; private set; } = new(0);
 
         public ReactivePropertySlim<string> BBBVal { get; private set; } = new(string.Empty);
@@ -25,13 +28,53 @@ namespace UI.UiWindow.MainWindow
         {
             _model = model;
 
+            ZZZVal = _model.AaaEntity.ToReactivePropertySlimAsSynchronized(
+                x => x.Value,
+                x => x.ZZZ.Value,
+                x =>
+                {
+                    var entity = _model.AaaEntity.Value;
+
+                    if (entity.IsHaveToCorrectAAA(x))
+                    {
+                        if (MessageBox.Show(
+                            "ZZZ設定の変更によりAAA設定の設定値が補正され、それに関わる項目も補正される可能性があります。\n\nZZZ設定を変更しますか？", 
+                            "確認",
+                            MessageBoxButton.YesNo,
+                            MessageBoxImage.Question,
+                            MessageBoxResult.No) == MessageBoxResult.Yes)
+                        {
+                            entity.SetZZZ(new(x), new AAAChangedEvent(_model.AaaEntity.Value, _model.BbbEntity.Value));
+                        }
+                    }
+                    else
+                    {
+                        entity.SetZZZ(new(x), new AAAChangedEvent(_model.AaaEntity.Value, _model.BbbEntity.Value));
+                    }
+
+                    _model.ForceNotifyAaaEntity();
+                    _model.ForceNotifyBbbEntity();
+
+                    return entity;
+                },
+                mode: ReactivePropertyMode.DistinctUntilChanged)
+                .AddTo(_compositeDisposable);
+
             AAAVal = _model.AaaEntity.ToReactivePropertySlimAsSynchronized(
                 x => x.Value,
                 x => x.AAA.Value,
                 x =>
                 {
                     var entity = _model.AaaEntity.Value;
-                    entity.SetAAA(new(x), new AAAChangedEvent(_model.AaaEntity.Value, _model.BbbEntity.Value));
+
+                    if (entity.IsOverZZZ(x))
+                    {
+                        MessageBox.Show("ZZZ設定を超える値を設定できません。");
+                    }
+                    else
+                    {
+                        entity.SetAAA(new(x), new AAAChangedEvent(_model.AaaEntity.Value, _model.BbbEntity.Value));
+                    }
 
                     _model.ForceNotifyAaaEntity();
                     _model.ForceNotifyBbbEntity();
